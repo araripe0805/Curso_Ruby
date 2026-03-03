5dc6ea4a83887e2444c5e2b856afd43d60cff62c
Inacio Araripe <araripeinacio@gmail.com>
2026-03-03 08:48:02 -0300

    Basico de Ruby: Exibir mensagem, variaveis e operadores

---
+++ 4.1/basico_ruby.rb
@@ -0,0 +1,25 @@
+#put string
+#Basicamente, serve para exibir algo no terminal
+puts 1 #1
+puts "a" #a
+
+#variaveis
+qualquer = 2
+puts qualquer
+
+#Interpolação de strings
+fruta = "morango"
+puts "Eu gosto de #{fruta}"
+
+#Operadores
+puts 2*2
+puts 3/3
+puts 1+1
+puts 2-2
+
+taxa = 3
+preco = 30
+valor = preco + taxa
+puts "O produto custa R$ #{valor}"
+#ou
+puts "O produto custa R$ #{taxa + preco}"
