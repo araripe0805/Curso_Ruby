0a17688c3832268ce43a0e07499283e28d0dac1d
Inacio Araripe <araripeinacio@gmail.com>
2026-03-10 08:48:47 -0300

    Herança, Override e Super

--- 16.1/Classes_Objetos_2.rb
+++ 16.1/Classes_Objetos_2.rb
@@ -76,3 +76,65 @@ class Cachorro < Animal
 end
 
 Cachorro.som #Au Au
+
+#Exemplo
+class Pizza
+  attr_reader sabor,preco
+
+  def initialize(sabor,preco)
+    @sabor = sabor
+    @preco = preco
+  end
+end
+
+class Mussarela < Pizza
+  
+  #Override
+  def initialize(preco)
+    #Compartilhamento de variaveis de instancias da superclasse para as filhas
+    @sabor = "mussarela"
+    @preco = preco
+  end
+end
+
+pizza = Mussarela.new(35)
+pizza.sabor #mussarela
+pizza.preco #35
+
+pizza_generica = Pizza.new("mussarela com azeitona", 38)
+pizza_generica.sabor #mussarela com azeitona
+
+#Super
+class Pizza2
+  attr_reader :sabor, :preco
+
+  def initialize(sabor, preco)
+    @sabor = sabor
+    @preco = preco
+  end
+end
+
+class Mussarela2 < Pizza2
+  def initialize(preco)
+    super("mussarela",preco)
+  end
+end
+
+pizza_mussarela = Mussarela2.new(38)
+pizza_mussarela.sabor #mussarela
+pizza_mussarela.preco #38
+
+#Exemplo
+class Pizza_Basica
+  def self.preco
+    35
+  end
+end
+
+Pizza_Basica.preco #35
+
+class PizzaCalabresa < Pizza_Basica
+  def self.preco
+    super + 5
+  end
+end
